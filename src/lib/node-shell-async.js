f4af6a317035b05864d6030cc84942ed649ea96a
LeeHyungGeun <lhg4031@gmail.com>
2019-10-09 16:24:12 +0900

    Add APIs: df, pwd, ls, file, less, cp

--- src/lib/node-shell-async.js
+++ src/lib/node-shell-async.js
@@ -1,26 +1,30 @@
-const cmd = require('node-cmd');
+import NodeCmdPromise from './node-cmd-promise';
+const cmd = new NodeCmdPromise();
+
 export class NodeShell {
   date() {
-    return new Promise((resolve, reject) => {
-      cmd.get('date', (err, data, stderr) => {
-        if (err) {
-          reject(err);
-        }
-
-        resolve(data);
-      });
-    });
+    return cmd.get('date');
   }
   cal() {
-    return new Promise((resolve, reject) => {
-      cmd.get('cal', (err, data, stderr) => {
-        if (err) {
-          reject(err);
-        }
-
-        resolve(data);
-      });
-    });
+    return cmd.get('cal');
+  }
+  df() {
+    return cmd.get('df');
+  }
+  pwd() {
+    return cmd.get('pwd');
+  }
+  ls(options = '') {
+    return cmd.get(`ls ${options}`);
+  }
+  file(filepath) {
+    return cmd.get(`file ${filepath}`);
+  }
+  less(filepath) {
+    return cmd.get(`less ${filepath}`);
+  }
+  cp(from, to, options = '') {
+    return cmd.get(`cp ${from} ${to} ${options}`);
   }
 }
 
